ee0d292dc2725f11a43b4707a64f5d45dcaf5b1d
Brandoxo <brandoxo90@gmail.com>
2025-07-17 17:20:20 -0600

    BUILD\ 170725 0520

--- resources/views/front/index/Sections/hotels.blade.php
+++ resources/views/front/index/Sections/hotels.blade.php
@@ -23,26 +23,26 @@
         <!-- Artículo 2 -->
         <article class="p-2 relative hover:scale-100"  data-aos="zoom-in">
             <div class="w-full h-64 overflow-hidden">
-                <a href="http://fch-expo.fchminerva.com/" class="opacity-80 transition-all duration-500 ease-in-out hover:opacity-100 hover:contrast-125 hover:cursor-pointer">
+                <a href="http://expo.fch-hotels.com/" class="opacity-80 transition-all duration-500 ease-in-out hover:opacity-100 hover:contrast-125 hover:cursor-pointer">
                     <img class="object-cover w-full h-full" src="{{ asset('assets/img/more-card/more-expo.webp') }}" alt="FCH Expo">
                 </a>
                 </div>
             <div class="text-white absolute px-4 -translate-y-16">
                 <h2 class="text-3xl font-extrabold overflow-hidden">FCH Expo</h2>
-                <a href="http://fch-expo.fchminerva.com"> <p class="text-base font-bold">Más Información</p></a>
+                <a href="http://expo.fch-hotels.com"> <p class="text-base font-bold">Más Información</p></a>
             </div>
         </article>
 
         <!-- Artículo 3 -->
         <article class="p-2 relative">
             <div class="w-full h-64 overflow-hidden"  data-aos="fade-left">
-                <a href="http://fch-provi.fchminerva.com" class="opacity-80 transition-all duration-500 ease-in-out hover:opacity-100 hover:contrast-125 hover:cursor-pointer">
+                <a href="http://provi.fch-hotels.com" class="opacity-80 transition-all duration-500 ease-in-out hover:opacity-100 hover:contrast-125 hover:cursor-pointer">
                     <img class="object-cover w-full h-full" src="{{ asset('assets/img/more-card/more-provi.webp') }}" alt="FCH Providencia">
                 </a>
                </div>
             <div class="text-white absolute px-4 -translate-y-16">
                 <h2 class="text-3xl font-extrabold overflow-hidden">FCH Providencia</h2>
-                <a href="http://fch-provi.fchminerva.com"> <p class="text-base font-bold">Más Información</p></a>
+                <a href="http://provi.fhc.hotels.com"> <p class="text-base font-bold">Más Información</p></a>
             </div>
         </article>
     </div>
